45f99419f5173c1c19c2d6dcf97fbd2cf1884e44
8wp <cianseangraham@gmail.com>
2026-02-22 20:32:16 +1100

    removed team section from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import IntegrationsSection from "@/components/integrations-3";
 import StatsSection from "@/components/stats";
-import TeamSection from "@/components/team";
 import CarouselSection, { CarouselSize } from "@/components/carousel";
 
 export default function Home() {
